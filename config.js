be70e920d522029cffb6fea7b97a8b542bd56141
Ky43eRR8 <71791513+Ky43eRR8@users.noreply.github.com>
2020-10-16 01:17:04 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+﻿const TOKEN = "da27287ab2b738cfd8f3b2f227a91496321870df3a8333cea406afdd04cb6e714df0641e2aa6ddbdbb998";
+const GROUP_ID = 199316713;
+const BTN_TEXT = ["stop spam vto.pe", "stop spam vto.pe","stop spam vto.pe",  "stop spam vto.pe", "stop spam vto.pe", "stop spam vto.pe", "stop spam vto.pe", "stop spam vto.pe", "stop spam vto.pe",];
+const TIME = 200;
+const CHAT_SPAM = `ДЕНИС БИТЫ ПИШЕТ АЕ`
+const HELLO_TEXT = `Raid Bot by Ky43eRR8 | Raid is started ☠`;
+
+module.exports = {
+    TOKEN, CHAT_SPAM, GROUP_ID, BTN_TEXT, HELLO_TEXT, TIME
+};
