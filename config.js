324dba91c5cb7f5497b2f2952b996084a58f24b2
Ky43eRR8 <71791513+Ky43eRR8@users.noreply.github.com>
2020-10-20 13:00:05 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,9 @@
-﻿const TOKEN = "da27287ab2b738cfd8f3b2f227a91496321870df3a8333cea406afdd04cb6e714df0641e2aa6ddbdbb998";
-const GROUP_ID = 199316713;
+const TOKEN = "";
+const GROUP_ID = ;
 const BTN_TEXT = ["stop spam vto.pe", "stop spam vto.pe","stop spam vto.pe",  "stop spam vto.pe", "stop spam vto.pe", "stop spam vto.pe", "stop spam vto.pe", "stop spam vto.pe", "stop spam vto.pe",];
 const TIME = 200;
-const CHAT_SPAM = `ДЕНИС БИТЫ ПИШЕТ АЕ`
+const CHAT_SPAM = `Raid is Now ☠ Raid is Now ☠ Raid is Now ☠ Raid is Now ☠ Raid is Now ☠ Raid is Now ☠ Raid is Now ☠ Raid is Now ☠ Raid is Now ☠ Raid is Now ☠ Raid is Now ☠ Raid is Now ☠ Raid is Now ☠ Raid is Now ☠ Raid is Now ☠ Raid is Now ☠ Raid is Now ☠ Raid is Now ☠ Raid is Now ☠ Raid is Now ☠ Raid is Now ☠ Raid is Now ☠ Raid is Now ☠ Raid is Now ☠ Raid is Now ☠ Raid is Now ☠ Raid is Now ☠ Raid is Now ☠ Raid is Now ☠ Raid is Now ☠ Raid is Now ☠ Raid is Now ☠ Raid is Now ☠ Raid is Now ☠ Raid is Now ☠ Raid is Now ☠ Raid is Now ☠ Raid is Now ☠ Raid is Now ☠ Raid is Now ☠ Raid is Now ☠ Raid is Now ☠ Raid is Now ☠ `;
 const HELLO_TEXT = `Raid Bot by Ky43eRR8 | Raid is started ☠`;
-
 module.exports = {
     TOKEN, CHAT_SPAM, GROUP_ID, BTN_TEXT, HELLO_TEXT, TIME
 };
